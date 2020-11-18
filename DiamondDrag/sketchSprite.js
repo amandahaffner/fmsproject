959c4c537c8efd6ac50393fd20c98b7f30623348
Trenton Ward <trenton.w52@gmail.com>
2020-11-17 17:38:45 -0700

    implemented rotating diamond objects

--- DiamondDrag/sketchSprite.js
+++ DiamondDrag/sketchSprite.js
@@ -1,11 +1,12 @@
 //This is the main program for the game "DiamondDrag"
-
+var diamondAnimation;
 var target, diamond1, diamond2, diamond3, draggedSprite;
 let inconsolata;
 var removedCounter = 0;
 
 function preload(){
-    inconsolata = loadFont('DDassets/Inconsolata-Regular.ttf');
+    diamondAnimation = loadAnimation("DDassets/diamondedited.png", "DDassets/diamondeditedsideways.png");
+    inconsolata = loadFont('DDassets/InconsolataUltraExpanded-ExtraBold.ttf');
 }
 function setup(){
     createCanvas(800, 400);
@@ -23,7 +24,10 @@ function setup(){
     diamond1.addImage(loadImage('DDassets/diamondedited.png'));
     diamond2.addImage(loadImage('DDassets/diamondedited.png'));
     diamond3.addImage(loadImage('DDassets/diamondedited.png'));
-    
+    //Add animations to each diamond object TODO: implement animations
+    //diamond1.addAnimation('moving', diamondAnimation);
+    //diamond2.addAnimation('moving', diamondAnimation);
+    //diamond3.addAnimation('moving', diamondAnimation);
     //Create collider for target
     target.setCollider('circle', 0, 0, 5);
     //Create colliders for each diamond
@@ -37,12 +41,15 @@ function setup(){
         if(draggedSprite == null){
             draggedSprite = this;
         }
+        //diamond1.changeAnimation('moving');
+        diamond1.rotation -= 90;
     }
     diamond1.onMouseReleased = function(){
         this.animation.goToFrame(0);
         if(draggedSprite == this){
             draggedSprite = null;
         }
+        diamond1.rotation += 90;
     }
 
     diamond2.onMousePressed = function(){
@@ -50,12 +57,14 @@ function setup(){
         if(draggedSprite == null){
             draggedSprite = this;
         }
+        diamond2.rotation -= 90;
     }
     diamond2.onMouseReleased = function(){
         this.animation.goToFrame(0);
         if(draggedSprite == this){
             draggedSprite = null;
         }
+        diamond2.rotation += 90;
     }
 
     diamond3.onMousePressed = function(){
@@ -63,12 +72,14 @@ function setup(){
         if(draggedSprite == null){
             draggedSprite = this;
         }
+        diamond3.rotation -= 90;
     }
     diamond3.onMouseReleased = function(){
         this.animation.goToFrame(0);
         if(draggedSprite == this){
             draggedSprite = null;
         }
+        diamond3.rotation += 90;
     }
 }
 
@@ -102,12 +113,14 @@ function endGame(){
     if((diamond1.removed == true) && (diamond2.removed == true) && (diamond3.removed == true)){
         target.remove();
         textFont(inconsolata);
-        textSize(width / 10);
+        textSize(width / 20);
         textAlign(CENTER, CENTER);
         //let time = millis(); //Leftover code for possible WEBGL implementation
         //rotateX(time / 1000);
         //rotateY(time / 1234);
         fill(34, 139, 34);
         text('YOU DID IT!', 250, 200);
+        textSize(width /40);
+        text('PLEASE REFRESH TO PLAY AGAIN.', 290, 230);
     }
 }
