f030224fc7102158672bb2e56db8d50b24bd350b
Trenton Ward <trenton.w52@gmail.com>
2020-11-30 19:14:22 -0800

    Made diamonds randomly locate upon start of game

--- DiamondDrag/sketchSprite.js
+++ DiamondDrag/sketchSprite.js
@@ -18,9 +18,12 @@ function setup(){
     target.addImage(loadImage('DDassets/rsz_targetimage.jpg'));
 
     //Create diamond sprite objects
-    diamond1 = createSprite(300, 150, 77, 77);
-    diamond2 = createSprite(150, 70, 77, 77);
-    diamond3 = createSprite(75, 300, 77, 77);
+    //diamond1 = createSprite(300, 150, 77, 77);
+    diamond1 = createSprite(random(width), random(height), 77, 77); //New randomized diamond locations
+    //diamond2 = createSprite(150, 70, 77, 77);
+    diamond2 = createSprite(random(width), random(height), 77, 77);
+    //diamond3 = createSprite(75, 300, 77, 77);
+    diamond3 = createSprite(random(width), random(height), 77, 77);
     //Add diamond image to diamond sprite objects
     diamond1.addImage(loadImage('DDassets/diamondedited.png'));
     diamond2.addImage(loadImage('DDassets/diamondedited.png'));
